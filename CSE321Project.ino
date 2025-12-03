961bb91aca0266229fae72694bbc515934f9080e
Kireeti Chakilam <kireetichakilam@gmail.com>
2025-12-03 16:35:00 -0500

    final commit

--- CSE321Project.ino
+++ CSE321Project.ino
@@ -22,3 +22,49 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Automated Light Logic Initialized");
 }
+
+void loop() {
+  int motionDetected = digitalRead(PIR_PIN);
+  int buttonState = digitalRead(BUTTON_PIN);
+
+  
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    overrideActive = true;
+    overrideStart = millis();
+    lightState = !lightState;
+  if (lightState) {
+      Serial.println("Manual ON");
+    } else {
+      Serial.println("Manual OFF");
+    }
+  } 
+  lastButtonState = buttonState;
+
+  // --- Override timer ---
+  if (overrideActive && (millis() - overrideStart > overrideDelay)) {
+    overrideActive = false;
+    Serial.println("Override ended, motion sensor reactivated");
+  }
+
+  // --- Motion-based control ---
+  if (!overrideActive) {
+    if (motionDetected == HIGH) {
+      lightState = true;
+      motionTimer = millis();
+      Serial.println("Motion detected, light ON");
+    } 
+    else if (lightState && (millis() - motionTimer > lightDuration)) {
+      lightState = false;
+      Serial.println("No motion, light OFF");
+    }
+  }
+
+  // --- Update LED to match light state ---
+  if (lightState == true) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  delay(50);
+}
